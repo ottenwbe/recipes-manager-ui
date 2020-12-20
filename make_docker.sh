700d3d49d977c5e489244ef3c886acbce18bc0fd
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2020-12-20 15:48:20 +0100

    push docker

--- make_docker.sh
+++ make_docker.sh
@@ -4,4 +4,4 @@ MAINTAINER="Beate Ottenwaelder <ottenwbe.public@gmail.com>"
 APP_VERSION=$(node -p -e "require('./package.json').version")
 DATE=$(date +"%F %T")
 
-docker build --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/go-cook-ui:${GO_COOK_UI_ARCH}-${APP_VERSION}" -f Dockerfile .
+docker build --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/go-cook-ui:${APP_VERSION}${GO_COOK_UI_ARCH}" -f Dockerfile .
--- push_docker.sh
+++ push_docker.sh
@@ -3,4 +3,4 @@
 APP_VERSION=$(node -p -e "require('./package.json').version")
 
 echo $(DOCKER_PASSWORD) | docker login -u $(DOCKER_USERNAME) --password-stdin
-docker push "ottenwbe/go-cook-ui:${GO_COOK_UI_ARCH}-${APP_VERSION}"
+docker push "ottenwbe/go-cook-ui:${APP_VERSION}${GO_COOK_UI_ARCH}"
