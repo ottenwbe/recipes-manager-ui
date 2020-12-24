20a3b628b1e724ce7590d8cfdc44fea789652bd2
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2020-12-24 23:05:34 +0100

    snapshot build supported

---
+++ make_snapshot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MAINTAINER="Beate Ottenwaelder <ottenwbe.public@gmail.com>"
+APP_VERSION=$(node -p -e "require('./package.json').version")
+DATE=$(date +"%F %T")
+GO_COOK_UI_ARCH="-$1"
+
+echo $(DOCKER_PASSWORD) | docker login -u $(DOCKER_USERNAME) docker.io --password-stdin
+docker build --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "docker.io/ottenwbe/go-cook-ui:SNAPSHOT${GO_COOK_UI_ARCH}" -f Dockerfile .
+docker push "docker.io/ottenwbe/go-cook-ui:SNAPSHOT${GO_COOK_UI_ARCH}"
