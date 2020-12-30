f37a34c8cec9e19d4a8f484ce367c7178059fc32
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2020-12-30 15:15:29 +0100

    buildx test

--- scripts/make_docker.sh
+++ scripts/make_docker.sh
@@ -4,4 +4,5 @@ MAINTAINER="Beate Ottenwaelder <ottenwbe.public@gmail.com>"
 APP_VERSION=$(node -p -e "require('./package.json').version")
 DATE=$(date +"%F %T")
 
+echo $2 | docker login -u $1 --password-stdin
 docker build --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/go-cook-ui:${APP_VERSION}${GO_COOK_UI_ARCH}" -f Dockerfile .
