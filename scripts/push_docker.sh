9da40b4f9808c92e9593e26590bb7b1472736de5
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2020-12-31 11:49:08 +0100

    push for all plattforms

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -3,4 +3,4 @@
 APP_VERSION=$(node -p -e "require('./package.json').version")
 
 echo $2 | docker login -u $1 --password-stdin
-docker push "ottenwbe/go-cook-ui:${APP_VERSION}${GO_COOK_UI_ARCH}"
+docker buildx push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 "ottenwbe/go-cook-ui:${APP_VERSION}${GO_COOK_UI_ARCH}"
