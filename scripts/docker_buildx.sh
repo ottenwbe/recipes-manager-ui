7648b67ef1f8d0bb9b72d2fadb67fd8202b5fe33
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2020-12-31 12:23:27 +0100

    push for all plattforms

---
+++ scripts/docker_buildx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MAINTAINER="Beate Ottenwaelder <ottenwbe.public@gmail.com>"
+APP_VERSION=$(node -p -e "require('./package.json').version")
+DATE=$(date +"%F %T")
+
+echo $2 | docker login -u $1 --password-stdin
+docker buildx build $3 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/go-cook-ui:${APP_VERSION}-test" -f Dockerfile .
--- scripts/make_docker.sh
+++ scripts/make_docker.sh
@@ -5,4 +5,4 @@ APP_VERSION=$(node -p -e "require('./package.json').version")
 DATE=$(date +"%F %T")
 
 echo $2 | docker login -u $1 --password-stdin
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/go-cook-ui:${APP_VERSION}-test" -f Dockerfile .
+docker build --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/go-cook-ui:${APP_VERSION}-test" -f Dockerfile .
