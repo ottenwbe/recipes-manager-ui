190fd2fb133c1939fb3f4d3234db03183e403dce
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2020-12-31 11:47:34 +0100

    build for all plattforms

--- scripts/make_docker.sh
+++ scripts/make_docker.sh
@@ -5,5 +5,4 @@ APP_VERSION=$(node -p -e "require('./package.json').version")
 DATE=$(date +"%F %T")
 
 echo $2 | docker login -u $1 --password-stdin
-docker buildx build --platform linux/arm/v7 --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/go-cook-ui:${APP_VERSION}${GO_COOK_UI_ARCH}" -f Dockerfile .
-#,linux/arm64/v8,linux/amd64
+docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/go-cook-ui:${APP_VERSION}${GO_COOK_UI_ARCH}" -f Dockerfile .
