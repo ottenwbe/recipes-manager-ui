7325be811a9a821deafc60cf2e1b03e3b6a23113
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2021-01-01 19:55:13 +0100

    revert test of push

--- scripts/docker_buildx.sh
+++ scripts/docker_buildx.sh
@@ -5,4 +5,4 @@ APP_VERSION=$(node -p -e "require('./package.json').version")
 DATE=$(date +"%F %T")
 
 echo $2 | docker login -u $1 --password-stdin
-docker buildx build $3 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/recipes-manager-ui:${APP_VERSION}-test" -f Dockerfile .
+docker buildx build $3 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/recipes-manager-ui:${APP_VERSION}" -f Dockerfile .
--- scripts/make_docker.sh
+++ scripts/make_docker.sh
@@ -5,4 +5,4 @@ APP_VERSION=$(node -p -e "require('./package.json').version")
 DATE=$(date +"%F %T")
 
 echo $2 | docker login -u $1 --password-stdin
-docker build --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/go-cook-ui:${APP_VERSION}${GO_COOK_UI_ARCH}" -f Dockerfile .
+docker build --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/recipes-manager-ui:${APP_VERSION}${GO_COOK_UI_ARCH}" -f Dockerfile .
