85b978701b2a82853f1833228ab6981c7f98af91
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2021-01-01 16:30:27 +0100

    add v infront of container name

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -3,4 +3,4 @@
 APP_VERSION=$(node -p -e "require('./package.json').version")
 
 echo $2 | docker login -u $1 --password-stdin
-docker push "ottenwbe/go-cook-ui:${APP_VERSION}${GO_COOK_UI_ARCH}"
+docker push "ottenwbe/go-cook-ui:v${APP_VERSION}${GO_COOK_UI_ARCH}"
