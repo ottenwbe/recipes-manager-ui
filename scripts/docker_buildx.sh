3d6c17d75c50a73cd768100cea524eb6f913848e
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2021-05-24 10:59:16 +0200

    with push

--- scripts/docker_buildx.sh
+++ scripts/docker_buildx.sh
@@ -4,5 +4,6 @@ MAINTAINER="Beate Ottenwaelder <ottenwbe.public@gmail.com>"
 APP_VERSION=$(node -p -e "require('./package.json').version")
 DATE=$(date +"%F %T")
 
-echo $2 | docker login -u $1 --password-stdin
-docker buildx build $3 --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/recipes-manager-ui:${APP_VERSION}" -f Dockerfile .
+SHOULD_PUSH=$1
+
+docker buildx build --output "type=image,push=${SHOULD_PUSH}" --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --label "version=${APP_VERSION}" --label "build_date=${DATE}"  --label "maintaner=${MAINTAINER}" -t "ottenwbe/recipes-manager-ui:${APP_VERSION}" -f Dockerfile .
