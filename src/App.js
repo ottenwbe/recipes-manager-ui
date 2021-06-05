4691ab79f45e6e32fa9b28557b43024af024af15
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2021-06-05 18:34:07 +0200

    Rework App layout

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import {
     Route,
     NavLink,
     HashRouter,
+    Redirect,
 } from "react-router-dom";
 
 import { Recipes, RandomRecipe } from './Recipes';
@@ -26,6 +27,21 @@ import { RecipeForm } from './RecipeForm';
 import { ThemeProvider } from 'react-bootstrap';
 import { createMuiTheme } from '@material-ui/core';
 
+import LocalDiningIcon from '@material-ui/icons/LocalDining';
+import StorageIcon from '@material-ui/icons/Storage';
+
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
 const { REACT_APP_APP_NAME } = process.env;
 
 class RecipesRouter extends Component {
@@ -33,6 +49,7 @@ class RecipesRouter extends Component {
         super(props);
         this.state = {
             numRecipes: 0,
+            open: false
         };
     }
 
@@ -51,9 +68,19 @@ class RecipesRouter extends Component {
         this.updateRecipes()
     }
 
+    handleDrawerOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleDrawerClose = () => {
+        this.setState({ open: false });
+    };
+
+
     render() {
         return (<HashRouter>
-            <RecipesRouterMenu numRecipes={this.state.numRecipes} />
+            <RecipesDrawer num={this.state.numRecipes} open={this.state.open} handleDrawerClose={this.handleDrawerClose} />
+            <RecipesRouterMenu open={this.state.open} numRecipes={this.state.numRecipes} handleDrawerOpen={this.handleDrawerOpen} />
             <RecipesRouterBody onRecipeCountChange={this.handleRecipeCountChange} />
         </HashRouter>);
     }
@@ -61,16 +88,18 @@ class RecipesRouter extends Component {
 
 const theme = createMuiTheme(
     {
-            primary: {
-                main: '#90caf9',
-            },
-            secondary: {
-                main: '#f50057',
-            },
+        primary: {
+            main: '#90caf9',
         },
+        secondary: {
+            main: '#f50057',
+        },
+    },
 );
 
 function RecipesRouterMenu(props) {
+
+    const classes = useStyles();
 
     const [searchTerm, setSearchTerm] = React.useState("");
 
@@ -85,25 +114,20 @@ function RecipesRouterMenu(props) {
 
     return (
         <div>
-            <CssBaseline />
+
             <ThemeProvider theme={theme}>
-                <AppBar style={{ backgroundColor: "#2196f3" }} className="RecipeMenu" position="fixed">
+                <AppBar style={{ backgroundColor: "#2196f3" }} position="fixed"
+                    className={clsx(classes.appBar, {
+                        [classes.appBarShift]: props.open,
+                    })}>
                     <Toolbar>
-                        <IconButton className="menu-icon" color="inherit" aria-label="Menu">
+                        <IconButton className="menu-icon" color="inherit" aria-label="Menu"
+                            onClick={props.handleDrawerOpen}>
                             <MenuIcon />
                         </IconButton>
                         <Typography variant="h6" color="inherit">
                             {REACT_APP_APP_NAME}
                         </Typography>
-                        <NavLink to="/"><Button><HomeIcon />News</Button></NavLink>
-                        <NavLink to="/recipes">
-                            <Badge badgeContent={props.numRecipes} color="secondary">
-                                <Button>Recipes</Button>
-                            </Badge>
-                        </NavLink>
-                        <NavLink to="/add"><Button>Add Recipe</Button></NavLink>
-                        <NavLink to="/rand"><Button>Random Recipe</Button></NavLink>
-                        <NavLink to="/src"><Button>Recipe Sources</Button></NavLink>
                         <div style={{ flexGrow: 1, }} />
                         <OutlinedInput
                             id="search-input"
@@ -134,11 +158,10 @@ function RecipesRouterBody(props) {
 
     return (<div className="GoCookUIContent" style={{ align: 'center', paddingRight: 50, paddingLeft: 50 }}>
         <div className="GoCookUIRoutes">
-            <Route exact path="/" component={Home} />
+            <Route exact path="/" ><Redirect to="/recipes" /></Route>
+            <Route path="/news" component={Home} />
             <Route exact path="/recipes" render={(props) => (<Recipes {...props} onRecipesChange={handleRecipeChange} />)} />
             <Route path="/recipes/:recipe" render={(props) => (<Recipes {...props} onRecipesChange={handleRecipeChange} />)} />
-            <Route path="/recipes/search/:search" render={(props) => (<Recipes {...props} onRecipesChange={handleRecipeChange} />)} />
-            <Route path="/recipes/similar/:similarTo" render={(props) => (<Recipes {...props} onRecipesChange={handleRecipeChange} />)} />
             <Route path="/add" render={(props) => (<RecipeForm {...props} onRecipesChange={handleRecipeChange} />)} />
             <Route path="/rand" component={RandomRecipe} />
             <Route path="/src" component={Sources} />
@@ -148,10 +171,142 @@ function RecipesRouterBody(props) {
     </div>);
 }
 
+function RecipesDrawer(props) {
+    const classes = useStyles();
+
+    const selectedItem = (hashName) => {
+        return window.location.hash === hashName ? true : false;
+    }
+
+    return (
+        <Drawer
+            className={classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={props.open}
+            onClose={props.handleDrawerClose}
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+        >
+            <div onClick={props.handleDrawerClose}>
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={props.handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    <NavLink to="/recipes" style={{ color: '#505050', textDecoration: 'none' }}>
+                        <ListItem button key="My Recipes" selected={selectedItem('#/recipes')}>
+                            <ListItemIcon>
+                                <LocalDiningIcon style={{ color: '#505050' }} />
+                            </ListItemIcon>
+                            <Badge badgeContent={props.num} color="secondary">
+                                <ListItemText primary="My Recipes      " />
+                            </Badge>
+                        </ListItem>
+
+                    </NavLink>
+                    <NavLink to="/rand" style={{ color: '#505050', textDecoration: 'none' }}>
+                        <ListItem button key="Random Recipes" selected={selectedItem('#/rand')}>
+                            <ListItemIcon></ListItemIcon>
+                            <ListItemText primary="Random Recipes" />
+                        </ListItem>
+                    </NavLink>
+                    <NavLink to="/add" style={{ color: '#505050', textDecoration: 'none' }}>
+                        <ListItem button key="Add Recipes" selected={selectedItem('#/add')}>
+                            <ListItemIcon></ListItemIcon>
+                            <ListItemText primary="Add Recipes" />
+                        </ListItem>
+                    </NavLink>
+                    <Divider />
+                    <NavLink to="/src" style={{ color: '#505050', textDecoration: 'none' }}>
+                        <ListItem button key="Sources" selected={selectedItem('#/sources')}>
+                            <ListItemIcon><StorageIcon style={{ color: '#505050' }} /></ListItemIcon>
+                            <ListItemText primary="Recipe Sources" />
+                        </ListItem>
+                    </NavLink>
+                    <Divider />
+                    <NavLink to="/news" style={{ color: '#505050', textDecoration: 'none' }}>
+                        <ListItem button key="News" selected={selectedItem('#/news')}>
+                            <ListItemIcon><HomeIcon style={{ color: '#505050' }} /></ListItemIcon>
+                            <ListItemText primary="News" />
+                        </ListItem>
+                    </NavLink>
+                </List>
+            </div>
+        </Drawer>
+    );
+}
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+}));
+
 function App() {
+
     return (
         <div className="GoCookUIApp">
+            <CssBaseline />
+
             <RecipesRouter />
+
         </div>
     );
 }
