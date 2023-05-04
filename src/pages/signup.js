454a5eba55da8ab2f565aa4aa4d1b59d8b087849
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2023-05-04 20:42:42 +0200

    signup page w/o calls to backend

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,42 +1,71 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import React, { Fragment, useContext } from 'react'
 import 'react-dom'
-import dynamic from 'next/dynamic'
-import { TextContextProvider, TextFromContext, textFromContext } from '@context/TextContextProvider'
-import { TextContext } from '@context/TextContext'
 import { Footer } from '@components/Footer'
 import { CssBaseline } from '@mui/material'
+import { TextContextComponent, TextContextProvider, TextFromContext, textFromContext } from '@context/TextContextProvider'
+import { TextContext } from '@context/TextContext'
+import { Container } from 'react-bootstrap'
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import KeyIcon from '@mui/icons-material/Key';
+import { Box } from '@mui/system'
 
-// DynamicApp -> disable server side rendering for now until child pages are refactored
-// currently document and window are used outside of 
-const DynamicApp = dynamic(() => import('@components/App'), {
-  loading: () => <div style={{ textAlign: 'center' }}>Loading...</div>,
-  ssr: false,
-})
+const Item = styled(Box)(({ theme }) => ({
+    background: 'linear-gradient(to right bottom, #000, #430089)',
+    color: '#fff',
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    flexGrow: 1,
+}));
 
 export default function Home() {
-  return (
-    <Fragment>
-      <TextContextProvider>
-        <HomeLayout />
-      </TextContextProvider>
-    </Fragment>
-  )
+    return (
+        <Fragment>
+            <TextContextProvider>
+                <HomeLayout />
+            </TextContextProvider>
+        </Fragment>
+    )
 }
 
 function HomeLayout() {
 
-  const texts = useContext(TextContext);
-  
-  return (
-    <Fragment>
-      <Head>
-        <title>{textFromContext(texts, 'appName')}</title>
-      </Head>
-      <CssBaseline />
-      <DynamicApp />
-      <Footer />
-    </Fragment>
-  )
+    const texts = useContext(TextContext);
+
+    const keyCloakSignUp = () => { 
+        console.log('click keycloak sign up')
+     };
+    const keyCloakLogin = () => { console.log('click keycloak log in') };
+
+    return (
+        <Fragment>
+            <Head>
+                <title>{textFromContext(texts, 'appName')}</title>
+            </Head>
+            <CssBaseline />
+            <Container>
+                <Box height="90vh" >
+                    <Stack height="90vh" alignItems="stretch" direction="column" useFlexGap flexWrap="wrap">
+                        <Item sx={{ width: '100%' }}>
+                            <h1><TextContextComponent value='appName' /></h1>
+                            <h2><TextContextComponent value='signupAd'/></h2>
+                            <p />
+                            <Button onClick={keyCloakSignUp} variant="contained" endIcon={<KeyIcon />}>
+                                <h3><TextContextComponent value='signupKeyCloak' /></h3>
+                            </Button>
+                            <p />
+                            <Button
+                                variant="text"
+                                onClick={keyCloakLogin} >
+                                Login with KeyCloak
+                            </Button>
+                        </Item>
+                    </Stack>
+                </Box>
+                <Footer />
+            </Container>
+        </Fragment>
+    )
 } 
