dbdd72afcb41145777171fd6585616a5512d129f
Beate Ottenwälder <b.ottenwaelder@gmail.com>
2024-01-21 20:13:59 +0100

    updated package.json and next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
+  /* config options here */
 }
-
 module.exports =  {
   experimental: {
     forceSwcTransforms: true,
